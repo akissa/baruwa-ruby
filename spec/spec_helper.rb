cf6fa25fe5672f0291d5c24ee4dd567d5e779b6c
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-16 11:43:53 +0200

    PKG: Remove codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,4 @@ require 'rspec'
 require 'webmock/rspec'
 require 'baruwa'
 
-ENV['CODECLIMATE_REPO_TOKEN'] = "69a832cbd11706e0578a20c2aae40d42cf520f3a9a776932bc32833dd1b58f2b"
-
-WebMock.disable_net_connect!(:allow_localhost => true, :allow => "codeclimate.com")
+WebMock.disable_net_connect!(:allow_localhost => true)
