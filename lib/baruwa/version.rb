7444b97453755d569798409f64be62444a8f60db
Andrew Colin Kissa <andrew@topdog.za.net>
2020-04-11 10:05:46 +0200

    PKG: Bump version for release

--- lib/baruwa/version.rb
+++ lib/baruwa/version.rb
@@ -1,11 +1,11 @@
 # -*- encoding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 # baruwa: Ruby bindings for the Baruwa REST API
-# Copyright (C) 2015-2019 Andrew Colin Kissa <andrew@topdog.za.net>
+# Copyright (C) 2015-2020 Andrew Colin Kissa <andrew@topdog.za.net>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 module Baruwa
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
